42b406a1ef7b49eaf4e29c00ae64110ce158157c
Eugene Rivera <eugenejrivera@gmail.com>
2019-10-01 19:56:12 -0700

    MR 10/1/19

--- pages/homeSales&Listings.vue
+++ pages/homeSales&Listings.vue
@@ -2,10 +2,10 @@
   <div class="container mx-auto py-20 px-6 sm:px-0 local-experts">
     <h1 class="py-4 header">Home Sales & Listings</h1>
     <div class="pdf  mb-4">
-      <a href="https://res.cloudinary.com/rivera-web-solutions/image/upload/v1565923955/BMK/MR_pdf.pdf" target="_blank">View PDF</a>
+      <a href="https://res.cloudinary.com/rivera-web-solutions/image/upload/v1569984659/BMK/MR_pdf.pdf" target="_blank">View PDF</a>
     </div>
     <a v-for="(page, index) in marketReport" :key="index"
-     href="https://res.cloudinary.com/rivera-web-solutions/image/upload/v1565923955/BMK/MR_pdf.pdf" target="_blank">
+     href="https://res.cloudinary.com/rivera-web-solutions/image/upload/v1569984659/BMK/MR_pdf.pdf" target="_blank">
       <img :src="page.src" :alt="page.alt">
     </a>
   </div>
@@ -26,7 +26,8 @@ export default {
         { 'src': 'https://res.cloudinary.com/rivera-web-solutions/image/upload/v1567649806/BMK/MR_4.jpg', 'alt': 'BMK Market Report Page 4' },
         { 'src': 'https://res.cloudinary.com/rivera-web-solutions/image/upload/v1567649805/BMK/MR_5.jpg', 'alt': 'BMK Market Report Page 5' },
         { 'src': 'https://res.cloudinary.com/rivera-web-solutions/image/upload/v1567649805/BMK/MR_6.jpg', 'alt': 'BMK Market Report Page 6' },
-        { 'src': 'https://res.cloudinary.com/rivera-web-solutions/image/upload/v1567649805/BMK/MR_7.jpg', 'alt': 'BMK Market Report Page 7' }
+        { 'src': 'https://res.cloudinary.com/rivera-web-solutions/image/upload/v1567649805/BMK/MR_7.jpg', 'alt': 'BMK Market Report Page 7' },
+        { 'src': 'https://res.cloudinary.com/rivera-web-solutions/image/upload/v1569984656/BMK/MR_8.jpg', 'alt': 'BMK Market Report Page 8' }
       ]
     }
   }
